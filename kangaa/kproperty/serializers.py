af747f56bc7f79b0aee82867dec7e05bab715154
tallosan <andrew.tallos@mail.utoronto.ca>
2017-01-26 21:48:15 -0500

    Update: Refactored create() method. Added secondary fields

--- kangaa/kproperty/serializers.py
+++ kangaa/kproperty/serializers.py
@@ -5,6 +5,9 @@ from rest_framework import serializers
 from kproperty.models import *
 
 
+#TODO: Refactor this. Move these secondary serializers elsewhere.
+## Secondary.
+'''   Serializer for Location models. '''
 class LocationSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -13,21 +16,79 @@ class LocationSerializer(serializers.ModelSerializer):
                    'address', 'postal_code',
                    'longitude', 'latitude')
 
+        
+class TaxRecordSerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model   = TaxRecord
+        fields  = ('assessment', 'assessment_year')
+
+
+class HistoricalSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model   = Historical
+        fields  = ('last_sold_price', 'last_sold_date',
+                   'year_built')
+        
+## Main.
 '''   Serializer for Property models. '''
 class PropertySerializer(serializers.ModelSerializer):
 
-    owner = serializers.ReadOnlyField(source='owner.username')
-    location = LocationSerializer()
+    owner       = serializers.ReadOnlyField(source='owner.username')
+    
+    # Primary fields.
+    location    = LocationSerializer()
+
+    # Secondary fields.
+    tax_records = TaxRecordSerializer()
+    history     = HistoricalSerializer()
     
     class Meta:
         model   = Property
         fields  = ('id', 'owner',
-                   'location',
                    'price',
                    'sqr_ftg',
-                   'n_bedrooms', 'n_bathrooms')
+                   'n_bedrooms', 'n_bathrooms',
+                   'location',
+                   'tax_records',
+                   'history')
 
+    ''' Handles the creation of a Property object.
+        N.B. -- It is up to the child classes to pass the correct property class
+                to this function.
+        Args:
+            property_class: The class of property we are creating (e.g. House).
+            validated_data: The request data we create the new model from.
+    '''
+    def create(self, property_class, validated_data):
+
+        # We need to explicitly declare these nested fields so we can create
+        # them later on.
+        nested_keys = {
+                        'location': Location(),
+                        'tax_records': TaxRecord(),
+                        'history': Historical()
+        }
+
+        # Go through the validated data, and create the nested objects using
+        # the given data. We then set the respective fields for our property object.
+        kproperty = property_class
+        for key in validated_data.keys():
+            if key in nested_keys.keys():
+                nested_data    = validated_data.pop(key)
+                instance_class = nested_keys[key].__class__
+                instance = instance_class.objects.create(**nested_data)
+                setattr(kproperty, key, instance)
+
+        # Now we pass through the regular (non-nested) field data.
+        setattr(kproperty, 'kwargs', validated_data)
+        for k, v in validated_data.iteritems():
+            setattr(kproperty, k, v)
+        
+        kproperty.save()
+        return kproperty
+ 
     ''' Update all target fields. Can handle nested objects.
         Args:
             instance: The actual object to be updated.
@@ -42,6 +103,8 @@ class PropertySerializer(serializers.ModelSerializer):
         for term in validated_data.keys():
             target_data = validated_data.pop(term)
             target = getattr(instance, term)
+            
+            # Nested object.
             if type(target_data) == collections.OrderedDict:
                 for field in target_data.keys():
                     setattr(target, field, target_data[field])
@@ -62,18 +125,10 @@ class CondoSerializer(PropertySerializer):
         model   = Condo
         fields  = PropertySerializer.Meta.fields + ('floor_num', )
 
-    #TODO: Pull this up into the parent?
-    ''' Handles the creation of a Condo object.
-        Args:
-            validated_data: The request data we create the new model from.
-    '''
+    ''' Overrides parent by passing a Condo model as the 'property_class'. '''
     def create(self, validated_data):
 
-        location_data   = validated_data.pop('location')
-        location        = Location.objects.create(**location_data)
-        condo           = Condo.objects.create(location=location, **validated_data)
-        
-        return condo
+        return super(CondoSerializer, self).create(Condo(), validated_data)
 
 
 '''   Serializer for House models. '''
@@ -82,17 +137,9 @@ class HouseSerializer(PropertySerializer):
     class Meta(PropertySerializer.Meta):
 
         model = House
-    
-    ''' Handles the creation of a House object.
-        Args:
-            validated_data: The request data we create the new model from.
-    '''
+   
+    ''' Overrides parent by passing a House model as the 'property_class'. '''
     def create(self, validated_data):
 
-        location_data   = validated_data.pop('location')
-        location        = Location.objects.create(**location_data)
-        house           = House.objects.create(location=location, **validated_data)
-        
-        return house
-
+        return super(HouseSerializer, self).create(House(), validated_data)
 
