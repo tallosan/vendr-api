c9f4e4dcc2c5e936271165e07c39aa03f1cb4ca0
tallosan <andrew.tallos@mail.utoronto.ca>
2017-01-30 18:16:05 -0500

    Added: One-to-one and Many-to-one fields.

--- kangaa/kproperty/models.py
+++ kangaa/kproperty/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from model_utils.managers import InheritanceManager
 
 
-#TODO: Add indexing to primary key / primary fields. (db_index=True)
 '''   Property model. Contains all data pertaining to a particular 
       Property instance. '''
 class Property(models.Model):
@@ -14,29 +13,18 @@ class Property(models.Model):
     objects     = InheritanceManager()
     
     # Owner of this property listing.
-    owner       = models.ForeignKey('auth.User',
-                    related_name='listings',
-                    on_delete=models.CASCADE)
+    owner       = models.ForeignKey('auth.User', related_name='listings',
+                    on_delete=models.CASCADE, db_index=True)
 
     # Main attributes.
-    price       = models.FloatField(blank=False)
-    sqr_ftg     = models.FloatField(blank=False)
-    n_bedrooms  = models.IntegerField(blank=False)
-    n_bathrooms = models.IntegerField(blank=False)
-    location    = models.ForeignKey('Location',
-                    related_name='location',
-                    on_delete=models.CASCADE, blank=False)
+    price       = models.FloatField(blank=False, db_index=True)
+    sqr_ftg     = models.FloatField(blank=False, db_index=True)
+    n_bedrooms  = models.IntegerField(blank=False, db_index=True)
+    n_bathrooms = models.IntegerField(blank=False, db_index=True)
     
-    # Secondary attributes.
-    history     = models.ForeignKey('Historical',
-                    related_name='history',
-                    on_delete=models.CASCADE, blank=True, null=True)
-
-    # Hidden fields (i.e. fields connected via a foreign key.
-    tax_records  = models.ForeignKey('TaxRecord',
-                    related_name='tax_records',
-                    on_delete=models.CASCADE, blank=True, null=True)
-
+    # Foreign key attributes.
+    # [ location, history, tax_records, features ]
+    
     ''' Returns the serializer type for this model. '''
     def get_serializer(self):
         raise NotImplementedError("'get_serializer()' must be implimented.")
@@ -67,28 +55,38 @@ class House(Property):
 
 
 ###  Detail objects. ###
-   
 '''   Location model. Contains locational data for a particular listing. '''
 class Location(models.Model):
 
-    country     = models.CharField(max_length=50, blank=False)
-    province    = models.CharField(max_length=20, blank=False)
-    city        = models.CharField(max_length=30, blank=False)
-    
-    address     = models.CharField(max_length=100, blank=False)
-    postal_code = models.CharField(max_length=10, blank=False)
-    
+    #kproperty   = models.ForeignKey('Property', related_name='location',
+                    #unique=True,
+                    #on_delete=models.CASCADE)
+    kproperty   = models.OneToOneField('Property', related_name='location',
+                    on_delete=models.CASCADE)
+
+    country     = models.CharField(max_length=50, blank=False, db_index=True)
+    province    = models.CharField(max_length=20, blank=False, db_index=True)
+    city        = models.CharField(max_length=30, blank=False, db_index=True)
+    address     = models.CharField(max_length=100, blank=False, db_index=True)
+    postal_code = models.CharField(max_length=10, blank=False, db_index=True)
     longitude   = models.DecimalField(max_digits=9, decimal_places=6, blank=False)
     latitude    = models.DecimalField(max_digits=9, decimal_places=6, blank=False)
 
 
 '''   Feature model. Contains the property features (e.g. fireplace, garden, etc). '''
 class Features(models.Model):
-    pass
+    
+    kproperty   = models.ForeignKey('Property', related_name='features',
+                    on_delete=models.CASCADE)
+    
+    feature     = models.CharField(max_length=50, blank=True)
 
 
-''' Tax model. Contains the tax data for the property. '''
+'''   Tax model. Contains the tax data for the property. '''
 class TaxRecord(models.Model):
+
+    kproperty   = models.ForeignKey('Property', related_name='tax_records',
+                    on_delete=models.CASCADE)
 
     # Most recent tax assessment, and the year in which it was made.
     assessment      = models.FloatField(blank=True, null=True)
@@ -98,10 +96,19 @@ class TaxRecord(models.Model):
 '''   Historical model. Contains data about the history of the property. '''
 class Historical(models.Model):
     
+    kproperty   = models.OneToOneField('Property', related_name='history',
+                    on_delete=models.CASCADE)
+
     # Price the property was last sold for, and the date it was sold.
     last_sold_price = models.FloatField(blank=True, null=True)
     last_sold_date  = models.DateField(blank=True, null=True)
 
     # The year the property was built.
     year_built      = models.IntegerField(blank=True, null=True)
+
+
+'''   Open house model. Contains scheduling info about open house / showing times. '''
+class OpenHouse(models.Model):
+
+    pass
 
