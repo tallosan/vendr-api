1693b4b1a79b4ac0c802e806cfb6e8caea985502
tallosan <andrew.tallos@mail.utoronto.ca>
2017-02-16 18:39:15 -0500

    Added: Custom user serializer. Implimented create() and update()

--- kangaa/kuser/serializers.py
+++ kangaa/kuser/serializers.py
@@ -1,23 +1,77 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
 from kproperty.models import Property
+from kuser.models import KUser, Profile
+
+User = get_user_model()
+
+
+'''   Serializer for Profile models. '''
+class ProfileSerializer(serializers.ModelSerializer):
+
+    prof_pic = serializers.ImageField(use_url=True)
+    
+    class Meta:
+        model   = Profile
+        fields  = ('first_name', 'last_name',
+                   'location',
+                   'prof_pic',
+                   'bio')
 
 
 '''   Serializer for User models. '''
 class UserSerializer(serializers.ModelSerializer):
 
-    # User listings, by Property ID.
-    listings = serializers.PrimaryKeyRelatedField(many=True,
+    profile  = ProfileSerializer(required=False)
+    listings = serializers.PrimaryKeyRelatedField(many=True, required=False,
                     queryset=Property.objects.select_subclasses())
 
-
     class Meta:
+        model   = KUser
+        fields  = ('id', #'href',
+                   'email', 'password',
+                   'profile',
+                   'listings',
+                   'join_date')
 
-        model   = User
-        fields  = ('id',
-                    'username', 'password',
-                    'email',
-                    'listings')
+    ''' Handles the creation of a Kangaa User object.
+        Args:
+            validated_data: The request data we create the new model from.
+    '''
+    def create(self, validated_data):
+
+        email       = validated_data.pop('email')
+        password    = validated_data.pop('password')
+        
+        kuser       = User.objects.create_user(email=email, password=password)
+
+        return kuser
+
+    ''' Update all target fields.
+        Args:
+            instance: The actual user object to be updated.
+            validated_data: Fields to be updated, and their updates.
+    '''
+    def update(self, instance, validated_data):
+        
+        for term in validated_data.keys():
+            target_data = validated_data.pop(term)
+            target = getattr(instance, term)
+            
+            # One-to-one relation update.
+            if type(target_data).__name__ == 'OrderedDict':
+                for field in target_data.keys():
+                    setattr(target, field, target_data[field])
+
+                target.save()
+            
+            # Regular field update.
+            else:
+                setattr(instance, term, key_data)
+        
+            instance.save()
+
+        return instance
 
