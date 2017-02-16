a28643326f5c7306a65ee486d42dea69e28504e2
tallosan <andrew.tallos@mail.utoronto.ca>
2017-02-16 18:41:44 -0500

    Added: Custom permission class for propertys.

---
+++ kangaa/kproperty/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework import permissions
+
+
+'''   Gives full permissions if the user is the owner of the posting, read
+      permissions if not. '''
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+
+        # If the request is safe (i.e. one that will not modify data), then
+        # we can return True.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Allow write permissions if the user is also the owner.
+        return obj.owner == request.user
+
