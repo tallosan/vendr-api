17929ce547177ce878c4f68cee4c9d877e262eb7
tallosan <andrew.tallos@mail.utoronto.ca>
2017-02-16 18:40:59 -0500

    Added: Custom feature serializers.

---
+++ kangaa/kproperty/nested_serializers.py
@@ -0,0 +1,46 @@
+#
+#   Serializers for nested models.
+#
+
+
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
+from kproperty.models import *
+
+
+'''   Serializer for Location models. '''
+class LocationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model   = Location
+        fields  = ('country', 'province', 'city',
+                   'address', 'postal_code',
+                   'longitude', 'latitude')
+
+
+'''   Serializer for Feature models. '''
+class FeaturesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model   = Features
+        fields  = ('feature', )
+
+
+'''   Serializer for Tax Record models. '''
+class TaxRecordSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model   = TaxRecord
+        fields  = ('assessment', 'assessment_year')
+
+
+'''   Serializer for Historical models. '''
+class HistoricalSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model   = Historical
+        fields  = ('last_sold_price', 'last_sold_date',
+                   'year_built')
+
