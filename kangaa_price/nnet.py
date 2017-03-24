eeb337f46b50fea87469b738af1a32b667c02de5
tallosan <andrew.tallos@mail.utoronto.ca>
2017-03-24 19:44:28 -0400

    Added: Custom arch0.

--- kangaa_price/nnet.py
+++ kangaa_price/nnet.py
@@ -25,12 +25,16 @@ class KNet(object):
         self.out  = T.dmatrix('out')
 
         self.network_in = nnet.layers.InputLayer(shape=inpt_shape, input_var=self.inpt)
-        self.l0         = nnet.layers.DenseLayer(self.network_in, num_units=100,
-                            nonlinearity=nnet.nonlinearities.tanh
+        self.netin_drop = nnet.layers.DropoutLayer(self.network_in, p=0.2)
+        self.l0         = nnet.layers.DenseLayer(self.netin_drop, num_units=40,
+                            nonlinearity=nnet.nonlinearities.rectify
         )
-        self.l1         = nnet.layers.DenseLayer(self.l0, num_units=50,
-                            nonlinearity=nnet.nonlinearities.tanh)
-        self.network    = nnet.layers.DenseLayer(self.l0, num_units=1,
+        self.l0_drop    = nnet.layers.DropoutLayer(self.l0, p=0.5)
+        self.l1         = nnet.layers.DenseLayer(self.network_in, num_units=20,
+                            nonlinearity=nnet.nonlinearities.rectify
+        )
+        self.l1_drop    = nnet.layers.DropoutLayer(self.l1, p=0.5)
+        self.network    = nnet.layers.DenseLayer(self.l1_drop, num_units=1,
                             nonlinearity=nnet.nonlinearities.linear
         )
 
@@ -49,20 +53,23 @@ class KNet(object):
         # Unpack the dataset, and determine the number of batches.
         x_train, y_train = dataset['training']
         x_val, y_val     = dataset['validation']
-        
+        '''
         # TODO: This changes the dataset.
-        x_train = x_train[:2]
-        y_train = y_train[:2]
-        x_val = x_val[:2]
-        y_val = y_val[:2]
+        i = 100
+        x_train = x_train[:i]
+        y_train = y_train[:i]
+        x_val = x_val[:i]
+        y_val = y_val[:i]
+        '''
+        n_train_batches = x_train.shape[0] / batch_size
+        n_val_batches   = x_val.shape[0] / batch_size
 
-        n_train_batches = x_train.shape.eval()[0] / batch_size
-        n_val_batches   = x_val.shape.eval()[0] / batch_size
-        
         # Cost expression. We'll use the squared error here.
+        # TODO: 2. Explore vanishing and exploding gradient problems
+        # TODO: 3. Dataset problem?
         pred = nnet.layers.get_output(self.network)
         loss = nnet.objectives.squared_error(pred, self.out)
-        loss = loss.mean()
+        loss = loss.mean() ** 0.5
 
         # Update rule. We'll use Nesterov momentum to speed things up.
         params  = nnet.layers.get_all_params(self.network, trainable=True)
@@ -89,13 +96,14 @@ class KNet(object):
             # Train the model.
             train_error, train_batches = 0, 0
             for batch_index in xrange(n_train_batches):
+                #import math
                 xs = x_train[batch_index * batch_size: (batch_index + 1) * batch_size]
                 ys = y_train[batch_index * batch_size: (batch_index + 1) * batch_size]
                 
                 # Reshape the label data, so that our NNet can use it.
                 ys = np.reshape(ys.eval(), ys.eval().shape + (1,))
                 
-                train_error += train_model(xs.eval(), ys)
+                train_error += train_model(abs(xs), ys)
                 train_batches += 1
             
             # Validate the model.
@@ -107,7 +115,7 @@ class KNet(object):
                 # Reshape the label data, so that our NNet can use it.
                 ys = np.reshape(ys.eval(), ys.eval().shape + (1,))
                 
-                val_error += val_model(xs.eval(), ys)
+                val_error += val_model(xs, ys)
                 val_batches += 1
 
             # Print feedback from the training.
@@ -122,6 +130,8 @@ class KNet(object):
 # Create the network and train it.
 #
 dataset = data.load_dataset()
+dataset = data.pre_process(dataset)
+
 network_in = {
                 'training': dataset[0],
                 'validation': dataset[1]
@@ -130,8 +140,8 @@ network_in = {
 # Hyperparameters:
 #
 EPOCHS      = 2000
-BATCH_SIZE  = 2
-ETA         = 0.0001
+BATCH_SIZE  = 200
+ETA         = 0.001
 RHO         = 0.9
 VERBOSE     = True
 
@@ -140,5 +150,4 @@ inpt_shape = (BATCH_SIZE, 3)
 knet = KNet(inpt_shape=inpt_shape)
 knet.train(dataset=network_in, epochs=EPOCHS, batch_size=BATCH_SIZE,
            eta=ETA, rho=RHO, verbose=True)
-
 
