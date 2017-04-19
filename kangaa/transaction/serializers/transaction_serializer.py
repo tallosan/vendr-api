ce3029e75142d791d9d06913fcd392948b3e200d
tallosan <andrew.tallos@mail.utoronto.ca>
2017-04-19 17:19:35 -0400

    Update: Implemented create and update functionality, along with custom representation for nested fields.

--- kangaa/transaction/serializers/transaction_serializer.py
+++ kangaa/transaction/serializers/transaction_serializer.py
@@ -18,8 +18,7 @@ class TransactionSerializer(serializers.ModelSerializer):
     
     buyer       = serializers.ReadOnlyField(source='buyer.id')
     
-    #TODO: Separate Buyer and Seller offers.
-    #TODO: Separate Buyer and Seller contracts.
+    # Calls 'get_offers' to separate buyer and seller offers.
     offers      = OfferSerializer(Offer.objects.all(), many=True)
     contracts   = ContractSerializer(Contract.objects.all(), many=True, required=False)
 
@@ -35,6 +34,12 @@ class TransactionSerializer(serializers.ModelSerializer):
                     'start_date',
         )
 
+    ''' Create a new Transaction. Each transaction begins/is created with a
+        single initial offer. Note, whilst the Offer is created here, we
+        defer Transaction creation to the Transaction Custom Manager.
+        Args:
+            validated_data: The data for the transaction.
+    '''
     def create(self, validated_data):
         
         # Get the buyer, seller, and 
@@ -52,14 +57,72 @@ class TransactionSerializer(serializers.ModelSerializer):
         # Create the offer.
         Offer.objects.create(owner=buyer, transaction=trans, **offer_data)
         
-        print 'created', trans
-        print 'created offer', trans.offers
-        print 'done creation'
         return trans
 
+    ''' Update a Transaction. We enforce field level permissions here.
+        Args:
+            instance: The Transaction model to update.
+            validated_data: The Transaction update data.
+    '''
     def update(self, instance, validated_data):
+
+        user = validated_data.pop('user')
         
-        print 'UPDATE: '
-        print instance
-        print validated_data
+        # Go through each given field, and perform an update.
+        for field in validated_data.keys():
+            target_data = validated_data.pop(field)
+            target = getattr(instance, field)
+            
+            # Handle one-to-one keys.
+            if type(target_data).__name__ == 'OrderedDict':
+                for _field in target_data.keys():
+                    setattr(target, _field, target_data[_field])
+
+                target.save()
+
+            # Handle foreign key fields.
+            elif type(target_data).__name__ == 'list':
+                for model_data in target_data:
+                    model_class = target.all().model
+                    model_class.objects.create(owner=user, transaction=instance,
+                            **model_data)
+
+            # Handle regular attributes.
+            else:
+                setattr(instance, field, target_data)
+        
+        return instance
+
+    ''' Transaction representation. Returns the offer data split into two separate
+        entities; buyer offers, and seller offers, along with the contract data
+        in the same manner.
+        Args:
+            instance: The Transaction model being serialized.
+    '''
+    def to_representation(self, instance):
+        
+        transaction = super(TransactionSerializer, self).to_representation(instance)
+        transaction['offers'] = self.format_offers(instance)
+              
+        return transaction
+
+    ''' (Helper) Format the Offers made by the buyer and seller involved in a
+        given transaction.
+        Args:
+            instance: The Transaction model being serialized.
+    '''
+    def format_offers(self, instance):
+        
+        buyer_id = instance.buyer.pk
+        seller_id = instance.seller.pk
+
+        buyer_offers  = instance.get_offers(user_id=buyer_id)
+        seller_offers = instance.get_offers(user_id=seller_id)
+
+        offers = [
+                    { "buyer_offers" : OfferSerializer(buyer_offers, many=True).data },
+                    { "seller_offers": OfferSerializer(seller_offers, many=True).data }
+        ]
+ 
+        return offers
 
