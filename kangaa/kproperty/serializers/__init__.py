a98ef085bcf5c3a93a0b90b014c591dfff5ed30a
tallosan <andrew.tallos@mail.utoronto.ca>
2017-05-03 18:58:53 -0400

    Update: Refactored serializers into separate files.

---
+++ kangaa/kproperty/serializers/__init__.py
@@ -0,0 +1,8 @@
+from .property_serializers import PropertySerializer, \
+                                  CoOpSerializer, CondoSerializer, \
+                                  FreeholdSerializer, HouseSerializer, \
+                                                      MultiplexSerializer
+from .property_field_serializers import LocationSerializer, FeaturesSerializer, \
+                                        TaxRecordsSerializer, HistoricalSerializer, \
+                                        ImagesSerializer
+
