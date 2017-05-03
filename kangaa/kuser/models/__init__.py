d3a89cb9504cb29a1f7209625d49222406c4c596
tallosan <andrew.tallos@mail.utoronto.ca>
2017-05-03 15:58:43 -0400

    Update: Migrate notification app into user app.

--- kangaa/kuser/models/__init__.py
+++ kangaa/kuser/models/__init__.py
@@ -1,3 +1,5 @@
+from .user import KUser, Profile
 from .notification import TransactionNotification, OfferNotification, ContractNotification
 from .notification import TransactionNotificationManager, OfferNotificationManager, \
                           ContractNotificationManager
+
