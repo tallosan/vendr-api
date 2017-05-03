fe226637f12deb54d01bfca9714cc7f5a492e4ec
tallosan <andrew.tallos@mail.utoronto.ca>
2017-05-03 13:43:10 -0400

    Added: User notifications to serializer.

--- kangaa/kuser/serializers.py
+++ kangaa/kuser/serializers.py
@@ -4,6 +4,12 @@ from rest_framework import serializers
 
 from kproperty.models import Property
 from kuser.models import KUser, Profile
+from transaction.models import Transaction
+from notification.models import *
+
+from transaction.serializers import TransactionSerializer
+
+from itertools import chain
 
 User = get_user_model()
 
@@ -11,7 +17,7 @@ User = get_user_model()
 '''   Serializer for Profile models. '''
 class ProfileSerializer(serializers.ModelSerializer):
 
-    prof_pic = serializers.ImageField(use_url=True)
+    prof_pic = serializers.ImageField(use_url=True, required=False)
     
     class Meta:
         model   = Profile
@@ -28,14 +34,25 @@ class UserSerializer(serializers.ModelSerializer):
     properties = serializers.PrimaryKeyRelatedField(many=True, required=False,
                     queryset=Property.objects.select_subclasses())
 
+    # Custom notification queryset.
+    notification_qs  = []
+    notification_qs += TransactionNotification.objects.all()
+    notification_qs += OfferNotification.objects.all()
+    notification_qs += ContractNotification.objects.all()
+    
+    notifications = serializers.PrimaryKeyRelatedField(many=True, required=False,
+                        queryset=notification_qs)
+
     class Meta:
         model   = KUser
         fields  = ('id', #'href',
-                   'email', #'password',
+                   'uuid',
+                   'email', 'password',
                    'profile',
                    'properties',
+                   'notifications',
                    'join_date')
-
+    
     ''' Handles the creation of a Kangaa User object.
         Args:
             validated_data: The request data we create the new model from.
@@ -46,7 +63,17 @@ class UserSerializer(serializers.ModelSerializer):
         password    = validated_data.pop('password')
         
         kuser       = User.objects.create_user(email=email, password=password)
+        
+        # Create the user profile if any data is given.
+        try:
+            prof = validated_data.pop('profile')
+            for key in prof.keys():
+                setattr(kuser.profile, key, prof.pop(key))
+            
+            kuser.profile.save()
 
+        except KeyError: pass
+        
         return kuser
 
     ''' Update all target fields.
@@ -74,4 +101,27 @@ class UserSerializer(serializers.ModelSerializer):
             instance.save()
 
         return instance
+
+
+    ''' User representation. Returns the transaction data, with the incoming
+        and outgoing transactions separated.
+        Args:
+            instance: The Transaction model being serialized.
+    '''
+    def to_representation(self, instance):
+
+        kuser = super(UserSerializer, self).to_representation(instance)
+        kuser['transactions'] = self.format_transactions(instance)
+        
+        return kuser
+
+    def format_transactions(self, instance):
+
+        user_id  = instance.pk
+        incoming = Transaction.objects.filter(seller=user_id).values_list('pk', flat=True)
+        outgoing = Transaction.objects.filter(buyer=user_id).values_list('pk', flat=True)
+
+        transactions = { "incoming": incoming, "outgoing": outgoing }
+
+        return transactions
 
