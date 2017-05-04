3630d84eab8b4c042d6acb0312421e48c2a4b3d6
tallosan <andrew.tallos@mail.utoronto.ca>
2017-05-04 19:22:56 -0400

    Update: Refactored serializer names.

--- kangaa/kuser/serializers/__init__.py
+++ kangaa/kuser/serializers/__init__.py
@@ -1,4 +1,5 @@
 from user_serializer import UserSerializer, ProfileSerializer
-from notification_serializer import TransactionSerializer, OfferSerializer, \
-                                    ContractSerializer
+from notification_serializer import TransactionNotificationSerializer, \
+                                    OfferNotificationSerializer, \
+                                    ContractNotificationSerializer
 
