3c0ab6348dd21473d225842eb5cce0bbfb3e4536
Ubuntu <ubuntu@ip-172-31-24-13.ca-central-1.compute.internal>
2017-08-07 21:54:59 +0000

    Added: Open house & RSVP endpoints.

--- vendr/kproperty/urls.py
+++ vendr/kproperty/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
         url(r'^(?P<pk>[0-9+]+)/$', views.PropertyDetail.as_view()),
 
         # Open house views.
-        url(r'^(?P<pk>[0-9+]+)/openhouse/$', views.OpenHouseList.as_view()),
-        url(r'^(?P<pk>[0-9+]+)/openhouse/(?P<oh_pk>[0-9a-f-]+)/$',
+        url(r'^(?P<pk>[0-9+]+)/openhouses/$', views.OpenHouseList.as_view()),
+        url(r'^(?P<pk>[0-9+]+)/openhouses/(?P<oh_pk>[0-9a-f-]+)/$',
             views.OpenHouseDetail.as_view()),
         
         # RSVP view.
-        url(r'^(?P<pk>[0-9+]+)/openhouse/(?P<oh_pk>[0-9a-f-]+)/rsvp/$',
+        url(r'^(?P<pk>[0-9+]+)/openhouses/(?P<oh_pk>[0-9a-f-]+)/rsvp/$',
             views.RSVPList.as_view()),
-        url(r'^(?P<pk>[0-9+]+)/openhouse/(?P<oh_pk>[0-9a-f-]+)'
+        url(r'^(?P<pk>[0-9+]+)/openhouses/(?P<oh_pk>[0-9a-f-]+)'
                              '/rsvp/(?P<rsvp_pk>[0-9a-f-]+)/$',
             views.RSVPDetail.as_view())
 ]
