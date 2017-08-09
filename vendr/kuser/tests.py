6ba2ae5fed8aa407e5ac2841f5548db3bab48914
Andrew Tallos <ubuntu@ip-172-31-24-13.ca-central-1.compute.internal>
2017-08-09 22:35:52 +0000

    Added: Messaging app tests. Still a work in progress though.

--- vendr/kuser/tests.py
+++ vendr/kuser/tests.py
@@ -5,7 +5,7 @@ from rest_framework.test import APIRequestFactory, APIClient, APITestCase
 from rest_framework.test import force_authenticate
 from rest_framework import status
 
-from kuser.models import KUser
+from kuser.models import *
 from kuser.views import *
 
 User = get_user_model()
@@ -43,7 +43,7 @@ class TestUserList(APITestCase):
     def test_create_duplicate_user(self):
 
         User.objects.create_user(email='t0@kangaa.xyz', password='test_pwd')
-        self.create_user(expected_status=status.HTTP_404_NOT_FOUND)
+        self.create_user(expected_status=status.HTTP_400_BAD_REQUEST)
 
 
 '''   Tests for UserList() view. '''
@@ -155,4 +155,183 @@ class TestUserDetail(APITestCase):
         self.delete_user(user=None,
                 path=self.user_a_path, uid=self.user_a_id,
                 expected_status=status.HTTP_401_UNAUTHORIZED)
+
+
+'''   Tests on the ChatList() view. '''
+class TestChatList(APITestCase):
+
+    def setUp(self):
+        
+        self.view = ChatList.as_view()
+        self.factory = APIRequestFactory()
+
+        # Create users.
+        self.user_a = User.objects.create(email='sender@vendr.xyz',
+                password='sender')
+        self.user_b = User.objects.create(email='recipient@vendr.xyz',
+                password='recipient')
+        self.chat_data = { 'participants': [self.user_b.pk] }
+ 
+        self.path_a = '/v1/users/{}/chat/'.format(self.user_a.pk)
+        self.path_b = '/v1/users/{}/chat/'.format(self.user_b.pk)
+
+    ''' (Helper) Creates a Chat object. '''
+    def create_chat(self, user, path):
+
+        request = self.factory.post(path, self.chat_data, format='json')
+        force_authenticate(request, user=user)
+        response = self.view(request, user.pk)
+
+        return response
+    
+    ''' Create a chat. '''
+    def test_create_chat(self):
+        
+        response = self.create_chat(user=self.user_a, path=self.path_a)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    ''' Ensure we can read chats on our endpoint. '''
+    def test_read_chat(self):
+        
+        chat_response = self.create_chat(user=self.user_a, path=self.path_a)
+        chat_data = chat_response.data
+        
+        request = self.factory.get(self.path_a, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_a.pk)
+
+        self.assertEquals(response.data[0]['pk'], chat_data['pk'])
+        self.assertEquals(response.data[0]['participants'],
+                chat_data['participants'])
+
+    ''' Ensure sender is included in participants implicitly. '''
+    def test_sender_included_in_participants(self):
+
+        request = self.factory.post(self.path_a, self.chat_data, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_a.pk)
+
+        self.assertItemsEqual(response.data['participants'],
+                [self.user_a.pk, self.user_b.pk])
+
+    ''' Ensure all desired participants are included. '''
+    def test_participants(self):
+
+        request = self.factory.post(self.path_a, self.chat_data, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_a.pk)
+        
+        # Ensure our recipient is included.
+        self.assertItemsEqual(response.data['participants'],
+                [self.user_a.pk, self.user_b.pk])
+        
+        # Ensure only our sender and recipient are included.
+        self.assertEqual(len(response.data['participants']), 2)
+
+    ''' Create a chat with another user on their endpoint. This should fail
+        as all chat related activities must be performed on a user's own
+        endpoint. '''
+    def test_create_chat_on_wrong_endpoint(self):
+        
+        request = self.factory.post(self.path_b, self.chat_data, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_b.pk)
+
+        self.assertEqual(response.status_code, 403)
+    
+    ''' Read a chat with another user on THEIR endpoint. This should fail, as
+        all chat related activites must be performed on a user's own endpoint.
+    '''
+    def test_read_chat_on_wrong_endpoint(self):
+
+        # Create the chat.
+        self.create_chat(user=self.user_a, path=self.path_a)
+
+        # Now, user A attempts to read on user B's endpoint.
+        request = self.factory.get(self.path_b, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_b.pk)
+        
+        self.assertEqual(response.status_code, 403)
+
+
+'''   Tests on the ChatDetail view. '''
+class TestChatDetail(APITestCase):
+
+    def setUp(self):
+        
+        self.view = ChatDetail.as_view()
+        self.factory = APIRequestFactory()
+
+        # Create users.
+        self.user_a = User.objects.create(email='sender@vendr.xyz',
+                password='sender')
+        self.user_b = User.objects.create(email='recipient@vendr.xyz',
+                password='recipient')
+ 
+        # Create our Chat.
+        self.chat = Chat.objects.create(participants=[self.user_a.pk,
+                        self.user_b.pk])
+        
+        self.path_a = '/v1/users/{}/chat/{}/'.format(self.user_a.pk, self.chat.pk)
+        self.path_b = '/v1/users/{}/chat/{}/'.format(self.user_b.pk, self.chat.pk)
+
+    ''' Read a chat on your own endpoint. '''
+    def test_read_chat(self):
+        '''
+        request = self.factory.get(self.path_a, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_a.pk, self.chat.pk)
+
+        #TODO: kwargs not passed through to the view!
+        assertEquals(response.status_code, 200)
+        assertEquals(response.data[0]['pk'], self.chat.pk)
+        assertEquals(response.data[0]['participants'], self.chat.participants)
+        '''
+    def test_read_chat_on_wrong_endpoint(self):
+        '''
+        #TODO: kwargs not passed through to the view!
+        request = self.factory.get(self.path_b, format='json')
+        force_authenticate(request, user=self.user_a)
+        response = self.view(request, self.user_b.pk, self.chat.pk)
+ 
+        assertEquals(response.status_code, 403)
+        '''
+
+class TestMessageList(APITestCase):
+
+    def setUp(self):
+
+        self.view = MessageList.as_view()
+        self.factory = APIRequestFactory()
+
+        # Create users.
+        self.user_a = User.objects.create(email='sender@vendr.xyz',
+                password='sender')
+        self.user_b = User.objects.create(email='recipient@vendr.xyz',
+                password='recipient')
+ 
+        # Create our Chat.
+        self.chat = Chat.objects.create(participants=[self.user_a.pk,
+                        self.user_b.pk])
+        
+        self.path_a = '/v1/users/{}/chat/{}/messages/'.\
+                      format(self.user_a.pk, self.chat.pk)
+        self.path_b = '/v1/users/{}/chat/{}/messages/'.\
+                      format(self.user_b.pk, self.chat.pk)
+
+    def test_create_message(self):
+        pass
+
+    def test_create_message_no_auth(self):
+        pass
+
+    def test_create_message_on_wrong_endpoint(self):
+        pass
+
+    def test_delete_message(self):
+        pass
+
+    def test_delete_message_on_wrong_endpoint(self):
+        pass
 
