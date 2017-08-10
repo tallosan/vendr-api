c28093ddb25ea8ad2de29dbb17b7776d6853b228
Andrew Tallos <ubuntu@ip-172-31-24-13.ca-central-1.compute.internal>
2017-08-10 21:00:04 +0000

    Update: Test client now covers authentication.

--- vendr/notify/test_client.js
+++ vendr/notify/test_client.js
@@ -1,17 +1,42 @@
-/* Test script for debugging notification socket. */
+/* Test script for debugging notification socket.
+ *
+ * Here, we connect a client to their notifications channel at:
+ *	
+ *	notifications.<user_pk>.<user_email>
+ *
+ * Note, we can also connect them to their inbox channel, although this
+ * isn't really covered in this example, by directing them to:
+ *
+ *	users.<user_pk>.inbox
+ *
+ * */
 
 var io = require('socket.io-client');
-var channel = 'notifications.pk.email';
+var notifications_channel = 'notifications.' + USER_PK + '.' + USER_EMAIL;
+// var inbox_channel = 'users.' + USER_PK + '.inbox';
+
+var token = OAUTH_TOKEN;
+var pk = USER_PK;
 
 // Note, we are explicitly setting the transport as websocket.
-var ws_config = {transports: ['websocket'], upgrade: false};
-var socket = io.connect('http://notify.zappme.xyz', ws_config);
+var ws_config = {
+	transports: ['websocket'],
+	upgrade: false,
+	extraHeaders: {
+		'Authorization': 'Bearer ' + token,
+		'pk': pk
+	}
+
+};
+
+var socket = io.connect('http://notify.vendr.xyz', ws_config);
 socket.on('connect', function(sock_data){
-	socket.emit('join', channel);
-	console.log('connected to ' + channel);
+	socket.emit('join', notifications_channel);
+	console.log('connected to ' + notifications_channel);
 });
 
-socket.on(channel, function(djson) {
+socket.on(notifications_channel, function(djson) {
+	console.log('\n');
 	console.log(djson);
 });
 
