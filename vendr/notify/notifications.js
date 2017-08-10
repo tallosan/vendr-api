d56212c37ffcd0c2991e940456cbfef0bd31e926
Andrew Tallos <ubuntu@ip-172-31-24-13.ca-central-1.compute.internal>
2017-08-10 21:01:52 +0000

    Added: Security features to protect live notify endpoints.

--- vendr/notify/notifications.js
+++ vendr/notify/notifications.js
@@ -7,10 +7,13 @@ const PORT = 9000;
 
 const REDIS_SERVER = 'localhost';
 const REDIS_PORT = 9200;
-const REDIS_NOTIFICATION_CHANNEL = 'notifications.*'
+
+// Our live channels cover both notifications, and messages.
+const REDIS_NOTIFICATION_CHANNEL = 'notifications.*';
+const REDIS_MESSAGES_CHANNEL = 'users.[0-9]+.inbox';
 
 const BASE_AUTH_URL = 'http://api.vendr.xyz/v1/users/'
-const BASE_AUTH_END = 'notifications/ws_auth/'
+const BASE_AUTH_END = '/notifications/ws_auth/'
 
 // Setup socket.io server.
 var app	= require('express')()
@@ -22,23 +25,30 @@ server.listen(PORT, function(){
 
 // Setup our Redis client.
 var redis_sub = require('redis').createClient(port=REDIS_PORT, host=REDIS_SERVER);
-redis_sub.psubscribe('notifications.*');
-
+redis_sub.psubscribe(REDIS_NOTIFICATION_CHANNEL);
+redis_sub.psubscribe(REDIS_MESSAGES_CHANNEL);
 
 /* Handle incoming HTTP requests. */
 app.get('/', function(req, res) {
 	res.send('<script src="/socket.io/socket.io.js"></script><script>var socket = io();</script>');
 });
-/*
-function auth(token, pk) {
+
+/* Authorize a request. */
+var request = require("request");
+function authenticate(token, pk) {
+	var options = {
+		url: BASE_AUTH_URL + pk + BASE_AUTH_END,
+		headers: {'authorization': token}
+	}
 	
-	var request = require('request');
-	var url = BASE_AUTH_URL + pk + BASE_AUTH_END;
-	request(url, function(error, response, body) {
-	
+	return new Promise(function(resolve, reject) {
+		request(options, function(error, response, body) {
+			resolve(response.statusCode);
+		});
 	});
+
 }
-*/
+
 /* Handle incoming websocket connections. */
 var clients = [];
 io.on('connection', function(socket){
@@ -47,17 +57,22 @@ io.on('connection', function(socket){
 	/* Connect client to appropriate channel. */
 	socket.on('join', function(channel) {
 
-		// Authenticate request.
-		console.log(socket.request.headers['authorization']);
-		console.log(socket.request.headers['pk']);
-		pk = socket.request.headers['pk'];
-		
-		console.log(channel);
-		console.log(BASE_AUTH_URL + pk + BASE_AUTH_END);
+		// Get authentication credentials.
+		var oauth_token = socket.request.headers['authorization'];
+		var pk = socket.request.headers['pk'];
 
-		socket.join(channel);
-		console.log('ws ' + socket.id + ' joined ' + channel);
-		socket.emit('greeting', 'welcome ' + socket.id + ' to ' + channel);
+		// Authenticate user. If they have the requisite credentials then
+		// they can join their channel. If not, we'll disconnect them.
+		response_code = authenticate(oauth_token, pk).then(function(response_code) {
+			if (response_code == 200) {
+				socket.join(channel);
+				console.log('ws ' + socket.id + ' joined ' + channel);
+				socket.emit('greeting', 'welcome ' + socket.id + ' to ' + channel);
+			}
+			else {
+				socket.disconnect();
+			}
+		});
 	});
 
 	/* Publish message on Redis push. */
