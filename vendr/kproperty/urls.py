cc42e2afb14a241d9ef5a32bb04c6e60c1fb31d5
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-08-17 22:54:13 +0000

    Added: Property Field views and their corresponding endpoints.

--- vendr/kproperty/urls.py
+++ vendr/kproperty/urls.py
@@ -7,6 +7,19 @@ urlpatterns = [
         url(r'^$', views.PropertyList.as_view()),
         url(r'^(?P<pk>[0-9+]+)/$', views.PropertyDetail.as_view()),
 
+        # Property Field views.
+        url(r'^(?P<pk>[0-9+]+)/features/$', views.FeaturesList.as_view()),
+        url(r'^(?P<pk>[0-9+]+)/features/(?P<ft_pk>[0-9+]+)/$',
+            views.FeaturesDetail.as_view()),
+
+        url(r'^(?P<pk>[0-9+]+)/taxrecords/$', views.TaxRecordsList.as_view()),
+        url(r'^(?P<pk>[0-9+]+)/taxrecords/(?P<tr_pk>[0-9+]+)/$',
+            views.TaxRecordsDetail.as_view()),
+
+        url(r'^(?P<pk>[0-9+]+)/images/$', views.ImagesList.as_view()),
+        url(r'^(?P<pk>[0-9+]+)/images/(?P<i_pk>[0-9+]+)/$',
+            views.ImagesDetail.as_view()),
+
         # Open house views.
         url(r'^(?P<pk>[0-9+]+)/openhouses/$', views.OpenHouseList.as_view()),
         url(r'^(?P<pk>[0-9+]+)/openhouses/(?P<oh_pk>[0-9a-f-]+)/$',
--- vendr/kproperty/views/__init__.py
+++ vendr/kproperty/views/__init__.py
@@ -1,3 +1,5 @@
 from property_views import PropertyList, PropertyDetail
+from property_field_views import FeaturesList, FeaturesDetail,\
+    TaxRecordsList, TaxRecordsDetail, ImagesList, ImagesDetail
 from open_house_views import OpenHouseList, OpenHouseDetail, RSVPList, RSVPDetail
 
