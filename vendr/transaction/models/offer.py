22b8bf51596162913fad9a9ae5caee13a054e07a
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-08-22 22:42:41 +0000

    Update: Removed unnecessary whitespace.

--- vendr/transaction/models/offer.py
+++ vendr/transaction/models/offer.py
@@ -11,18 +11,18 @@ from .transaction import Transaction
 '''   Model representing an offer on a property, or a counter-offer. '''
 class Offer(models.Model):
 
-    id          = models.UUIDField(primary_key=True, default=uuid.uuid4,
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                     editable=False)
 
     # The user who made the offer, and the transaction the offer belongs to.
-    owner       = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='offers',
-                    on_delete = models.CASCADE, db_index=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='offers',
+                    on_delete=models.CASCADE, db_index=True)
     transaction = models.ForeignKey(Transaction, related_name='offers',
                     on_delete=models.CASCADE)
     
-    offer       = models.FloatField()
-    deposit     = models.FloatField()
-    comment     = models.CharField(max_length=350, blank=True)
+    offer = models.FloatField()
+    deposit = models.FloatField()
+    comment = models.CharField(max_length=350, blank=True)
     
-    timestamp   = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     
