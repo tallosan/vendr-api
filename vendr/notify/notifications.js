6e5097044365579fe7de2ccc66f8d575a3238c93
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-08-29 18:23:41 +0000

    Fixed: Redis channel bug.  uses glob instead of regex, so I've refactored the channel names accordingly.

--- vendr/notify/notifications.js
+++ vendr/notify/notifications.js
@@ -8,12 +8,21 @@ const PORT = 9000;
 const REDIS_SERVER = 'localhost';
 const REDIS_PORT = 9200;
 
-// Our live channels cover both notifications, and messages.
-const REDIS_NOTIFICATION_CHANNEL = 'users.[0-9]+.notifications';
-const REDIS_MESSAGES_CHANNEL = 'users.[0-9]+.inbox';
+// Supported channels: Notifications & Inbox.
+// N.B. -- Redis uses glob style for pattern matching, NOT regex! This doesn't
+// give us much control over the inputted string, so we'll do what we can with
+// this pattern and leave the rest to SocketIO. The patterns below will match
+// any strings that being with 'users' and end with a supported channel. We can
+// check that the user pk at least beings with a digit, however glob doesn't
+// support matching for arbitrary length digits. Thus, the * wild card will have
+// to do here, with the unfortunate side effect that it'll also match characters.
+const REDIS_NOTIFICATION_CHANNEL_GLOB = 'users.[0-9]*.notifications';
+const REDIS_MESSAGES_CHANNEL_GLOB = 'users.[0-9]*.inbox';
 
 const BASE_AUTH_URL = 'http://api.vendr.xyz/v1/users/'
 const BASE_AUTH_END = '/ws_auth/'
+
+var Regex = require('regex');
 
 // Setup socket.io server.
 var app	= require('express')()
@@ -25,13 +34,24 @@ server.listen(PORT, function(){
 
 // Setup our Redis client.
 var redis_sub = require('redis').createClient(port=REDIS_PORT, host=REDIS_SERVER);
-redis_sub.psubscribe(REDIS_NOTIFICATION_CHANNEL);
-redis_sub.psubscribe(REDIS_MESSAGES_CHANNEL);
+redis_sub.psubscribe(REDIS_NOTIFICATION_CHANNEL_GLOB);
+redis_sub.psubscribe(REDIS_MESSAGES_CHANNEL_GLOB);
 
 /* Handle incoming HTTP requests. */
 app.get('/', function(req, res) {
 	res.send('<script src="/socket.io/socket.io.js"></script><script>var socket = io();</script>');
 });
+
+/* Ensure that the channel is valid. We don't want to waste time dealing with
+ * connections to a room we don't service. */
+function validate_channel_name(channel_name) {
+
+	var notification_regex = '^(?:users.[0-9]+.notifications)$';
+	var inbox_regex = '^(?:users.[0-9]+.inbox)$';
+	var valid_channel_regex = notification_regex + '|' +  inbox_regex;
+
+	return channel_name.match(valid_channel_regex) != null;
+}
 
 /* Authorize a request. */
 var request = require("request");
@@ -56,10 +76,22 @@ io.on('connection', function(socket){
 	/* Connect client to appropriate channel. */
 	socket.on('join', function(channel) {
 
-		// Get authentication credentials.
-		var oauth_token = socket.request.headers['authorization'];
-		var pk = socket.request.headers['pk'];
+		// Ensure the channel name is valid.
+		if (validate_channel_name(channel) === false) {
+			socket.disconnect();
+		}
 
+		// Get authentication credentials.
+		//var oauth_token = socket.request.headers.cookie['authorization'];
+		//var pk = socket.request.headers.cookie['pk'];
+		
+		//
+		socket.join(channel);
+		console.log('ws ' + socket.id + ' joined ' + channel);
+		socket.emit('greeting', 'welcome ' + socket.id + ' to ' + channel);
+		//
+
+		/*
 		// Authenticate user. If they have the requisite credentials then
 		// they can join their channel. If not, we'll disconnect them.
 		response_code = authenticate(oauth_token, pk).then(function(response_code) {
@@ -72,11 +104,13 @@ io.on('connection', function(socket){
 				socket.disconnect();
 			}
 		});
+		*/
 	});
 
 	/* Publish message on Redis push. */
 	redis_sub.on('pmessage', function(pattern, channel, message) {
 
+		console.log('mesasage');
 		// Strip whitespace. Channels must match exactly on both sides!
 		channel = channel.replace(/ /g, '');
 		console.log('pattern: ' + pattern + '\nchannel: ' + channel + '\nmessage: ' + message);
