53d4f1a1faf2f75cfa3a4fbbe9179ab0d73619af
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-08-30 22:36:41 +0000

    Update: Refactored to remove hard foreign key reference to Transaction.

--- vendr/transaction/models/offer.py
+++ vendr/transaction/models/offer.py
@@ -5,8 +5,6 @@ import uuid
 from django.db import models
 from django.conf import settings
 
-from .transaction import Transaction
-
 
 '''   Model representing an offer on a property, or a counter-offer. '''
 class Offer(models.Model):
@@ -17,7 +15,7 @@ class Offer(models.Model):
     # The user who made the offer, and the transaction the offer belongs to.
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='offers',
                     on_delete=models.CASCADE, db_index=True)
-    transaction = models.ForeignKey(Transaction, related_name='offers',
+    transaction = models.ForeignKey('Transaction', related_name='offers',
                     on_delete=models.CASCADE)
     
     offer = models.FloatField()
