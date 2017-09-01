e166721da708606c4a49a1c328d04c1f297407d1
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-09-01 19:35:56 +0000

    Added: Favourites serializer.

--- vendr/kuser/serializers/__init__.py
+++ vendr/kuser/serializers/__init__.py
@@ -4,6 +4,7 @@ from notification_serializer import TransactionNotificationSerializer, \
                                     ContractNotificationSerializer
 from chat_serializers import ChatSerializer, MessageSerializer
 from schedule_serializer import ScheduleSerializer
+from favourites_serializer import FavouritesSerializer
 from user_property_serializer import UserPropertySerializer
 from user_transaction_serializer import UserTransactionSerializer
 
---
+++ vendr/kuser/serializers/favourites_serializer.py
@@ -0,0 +1,21 @@
+#
+# Serializer for a User object's transactions.
+#
+# ========================================================================
+
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from transaction.models import Transaction
+
+User = get_user_model()
+
+
+class FavouritesSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = User
+        fields = ('favourites', )
+
