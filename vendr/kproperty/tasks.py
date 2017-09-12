6830923ae70ec3569f4fff209ec77aac9a893a24
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-09-12 19:53:22 +0000

    Added: Tasks for unfeaturing properties and open house deletion.

--- vendr/kproperty/tasks.py
+++ vendr/kproperty/tasks.py
@@ -1,8 +1,28 @@
+#
+# Periodic tasks for Property objects and related models.
+#
+# ================================================================
+
 from __future__ import absolute_import
 from celery import shared_task
 
-@shared_task
-def unfeature(eta):
+from kproperty.models import Property, OpenHouse, RSVP
 
-    return 'this task will be unfeatured in {}'.format(eta)
+
+""" Unfeature a property after a day. """
+@shared_task
+def property_unfeature_task():
+    
+    for kproperty in Property.objects.unfeature_queue():
+        kproperty.is_featured = False
+        kproperty.save()
+
+
+""" Perform a soft-delete on open house models after they have
+    been completed. """
+@shared_task
+def openhouse_clear_task():
+
+    #TODO: Pending design.
+    pass
 
