355f974ac01fe95dccdafaf8e92471d3260ccfaa
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-09-12 19:52:22 +0000

    Added: Celery setup.

--- vendr/vendr/celery.py
+++ vendr/vendr/celery.py
@@ -1,16 +1,33 @@
 from __future__ import absolute_import, unicode_literals
 import os
-'''
 from celery import Celery
+from celery.schedules import crontab
 
 from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kangaa.settings')
-app = Celery('kangaa')
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vendr.settings')
+app = Celery('vendr')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-'''
+
+# Application schedule.
+app.conf.beat_schedule = {
+
+        # Check for properties to unfeature every 2 hours.
+        'property_unfeature': {
+            'task': 'kproperty.tasks.property_unfeature_task',
+            'schedule': crontab(hour='*/2')
+        },
+        
+        # Check for open houses to clear every 2 hours.
+        'openhouse_clear': {
+            'task': 'kproperty.tasks.openhouse_clear_task',
+            'schedule': crontab(hour='*/2')
+        }
+}
+
