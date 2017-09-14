d17dfe126b0d4d3754055bb404d81f08e32fa247
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-09-14 20:19:57 +0000

    Added: .png extension to images.

--- vendr/custom_storage/aws/s3.py
+++ vendr/custom_storage/aws/s3.py
@@ -7,18 +7,34 @@ class S3Adapter(object):
         self.location = location
         
         self.resource = 's3'
+        self.extension = 'png'
         self.s3 = boto3.resource(self.resource)
         
+    """ Save the file to our s3 bucket.
+        Args:
+            name (str) -- The generated key for our file.
+            content (file) -- The actual file we're uploading.
+    """
     def save(self, name, content):
         
         bucket_name = self.location
         bucket = self.s3.Bucket(bucket_name)
         
+        name = '{}.{}'.format(name, self.extension)
         response = bucket.put_object(Key=name, Body=content)
-        
-        name = self._format_name(bucket=response._bucket_name, key=response._key)
+
+        name = self._format_name(bucket=response._bucket_name,
+                key=response._key)
+
         return name
 
+    """ S3 is returning only parts of the resource URL, so if we
+        want the full URL for our Image's `name` field then we'll
+        have to reconstruct / format the parts ourselves.
+        Args:
+            bucket (str) -- The s3 bucket name.
+            key (str) -- The key of the file we just uploaded.
+    """
     def _format_name(self, bucket, key):
 
         BASE = 'https://s3.ca-central-1.amazonaws.com/'
