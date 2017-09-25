316aad25c237850639c865c12e3c46be3d58a0a2
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-09-25 20:34:58 +0000

    Added: Models for document clause types.

--- vendr/transaction/models/__init__.py
+++ vendr/transaction/models/__init__.py
@@ -2,5 +2,7 @@ from .transaction import Transaction
 from .offer import Offer
 from .contract import *
 from .closing import Closing, Document, Amendments, Waiver, \
-        NoticeOfFulfillment, MutualRelease, DocumentClause, AbstractClosingFactory
+        NoticeOfFulfillment, MutualRelease, DocumentClause, AbstractClosingFactory, \
+        AmendmentDocumentClause, WaiverDocumentClause, \
+        NoticeOfFulfillmentDocumentClause
 
