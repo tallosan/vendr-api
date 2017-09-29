4ac09db3e4e2a9e82d0e2f8051a2577d78a5ce6a
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2017-09-29 22:57:56 +0000

    Added: Custom manager for OpenHouse models so that we can create custom querysets without any overhead.

--- vendr/kproperty/models/open_house.py
+++ vendr/kproperty/models/open_house.py
@@ -4,15 +4,35 @@
 # ================================================================
 
 from __future__ import unicode_literals
+from datetime import datetime, timedelta
+
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 import uuid
+
+class OpenHouseManager(models.Manager):
+
+    """ Queue of Open Houses that are starting 'soon', where we define
+        soon to mean 'in x hours'. """
+    def starting_soon_queue(self):
+
+        # Determine which open houses start in the next `notify_before`
+        # window (defaults to 1 hour before).
+        notify_before = timedelta(hours=1)
+        notify_window = timezone.now() + notify_before
+        starting_soon_queue = super(OpenHouseManager, self).get_queryset().\
+                filter(start__gte=timezone.now()).\
+                filter(start__lte=notify_window)
+
+        return starting_soon_queue
 
 
 """   Container for RSVP models. """
 class OpenHouse(models.Model):
 
+    objects = OpenHouseManager()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
             editable=False, db_index=True)
     
@@ -24,6 +44,8 @@ class OpenHouse(models.Model):
     # The start & end of the open house.
     start = models.DateTimeField()
     end   = models.DateTimeField()
+
+    _recipients_notified = models.BooleanField(default=False)
 
 
 """   Represents a user's RSVP to an open house. """
