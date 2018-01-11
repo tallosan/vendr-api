ec171877348d94f1fda6c7e8dc8d3912e28dc39b
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2018-01-11 23:41:26 +0000

    Update: Serializers now handle Account creation and reads.

--- vendr/payment/serializers/payment_serializers.py
+++ vendr/payment/serializers/payment_serializers.py
@@ -1,13 +1,17 @@
 #
 # Payment serializers.
 #
-# @author :: tallosanI
+# @author :: tallosan
 # ================================================================
 
 from rest_framework import serializers
 
-from kuser.models import KUser as User
+from django.contrib.auth import get_user_model
+
+from kuser.serializers import GenericAccountSerializer
 from payment.models import Payment
+
+User = get_user_model()
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -19,6 +23,10 @@ class PaymentSerializer(serializers.ModelSerializer):
             required=False,
             queryset=User.objects.all()
     )
+    recipient = serializers.PrimaryKeyRelatedField(
+            required=False,
+            queryset=User.objects.all()
+    )
 
     class Meta:
         model = Payment
@@ -26,8 +34,31 @@ class PaymentSerializer(serializers.ModelSerializer):
                 "pk",
                 "payee",
                 "recipient",
+                "payee_account",
+                "recipient_account",
                 "amount",
                 "message",
-                "timestamp"
+                "timestamp",
+        )
+
+    def update(self, instance, validated_data):
+        """
+        Get the given Account models (if any), and set them.
+        """
+
+        # Payee account.
+        payee_account = validated_data.pop("payee_account", None)
+        if payee_account:
+            instance.payee_account = payee_account
+
+        # Recipient account.
+        recipient_account = validated_data.pop("recipient_account", None)
+        if recipient_account:
+            instance.recipient_account = recipient_account
+
+        instance.save()
+        return super(PaymentSerializer, self).update(
+                instance,
+                validated_data
         )
 
