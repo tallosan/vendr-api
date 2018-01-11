cdc3ce5819bdc755ca3ac585ba1bdc50a641941b
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2018-01-11 23:40:44 +0000

    Update: Finished payment view.

--- vendr/payment/urls.py
+++ vendr/payment/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
         url(r"^$", views.PaymentList.as_view()),
-        url(r"^(?P<transaction_pk>[0-9a-f-]+)/$", views.PaymentDetail.as_view()),
+        url(r"^(?P<payment_pk>[0-9a-f-]+)/$", views.PaymentDetail.as_view()),
 ]
 
--- vendr/payment/views/payment_views.py
+++ vendr/payment/views/payment_views.py
@@ -1,13 +1,17 @@
 #
 # Payments API.
 #
-# @author :: tallosanI
+# @author :: tallosan
 # ================================================================
 
 from rest_framework import status
-from rest_framework.generics import (CreateAPIView, RetrieveUpdateAPIView)
 from rest_framework.response import Response
+from rest_framework.generics import (
+        CreateAPIView,
+        RetrieveUpdateDestroyAPIView
+)
 
+from kuser.models import BaseAccount
 from payment.models import Payment
 from payment.serializers import PaymentSerializer
 
@@ -17,21 +21,28 @@ class PaymentList(CreateAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
-    def post(self, request, format=None):
+    def post(self, request, *args, **kwargs):
 
         payee = request.user
         assert payee, (
                 "error: no payee given."
         )
 
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(payee=payee)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        request.data["payee"] = payee.pk
+        return super(PaymentList, self).post(request, *args, **kwargs)
 
 
-class PaymentDetail(RetrieveUpdateAPIView):
-    pass
+class PaymentDetail(RetrieveUpdateDestroyAPIView):
+    
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    lookup_url_kwarg = "payment_pk"
+
+    def update(self, request, *args, **kwargs):
+        """
+        Override to allow partial updates.
+        """
+
+        kwargs["partial"] = True
+        return super(PaymentDetail, self).update(request, *args, **kwargs)
 
