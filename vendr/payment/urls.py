49e30a1dcd8a2a31bf76ae7cdf964cb447c2b7db
Andrew Tallos <andrew.tallos@mail.utoronto.ca>
2018-01-13 21:18:40 +0000

    Added:  and  resources.

--- vendr/payment/urls.py
+++ vendr/payment/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
         url(r"^$", views.PaymentList.as_view()),
         url(r"^(?P<payment_pk>[0-9a-f-]+)/$", views.PaymentDetail.as_view()),
+        url(r"^(?P<payment_pk>[0-9a-f-]+)/deposit/$",
+            views.PaymentDeposit.as_view()),
+        url(r"^(?P<payment_pk>[0-9a-f-]+)/forward/$",
+            views.PaymentForward.as_view()),
 ]
 
